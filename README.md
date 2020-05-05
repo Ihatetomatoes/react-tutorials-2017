80c43f9362576f76ed7afe62243fe876bccd070d
Petr Tichy <peter@ihatetomatoes.net>
2020-05-06 07:12:17 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# React Tutorials 2017 - Starter Files
+
+## How to use the files?
+
+1. Clone this repo
+2. Run `npm install` to install all the dependencies
+3. Run `npm run dev`
+4. New browser window should open with a green text on pink background.
+
+Don't forget to [subscribe to my channel](https://www.youtube.com/channel/UC7O6CntQoAI-wYyJxYiqNUg?sub_confirmation=1) for more front-end videos.
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require("path");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/, 
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader','sass-loader'],
+                    publicPath: '/dist'
+                })
+            },
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            // minify: {
+            //     collapseWhitespace: true
+            // },
+            hash: true,
+            template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+        }),
+        new ExtractTextPlugin({
+            filename: 'app.css',
+            disable: false,
+            allChunks: true
+        })
+    ]
+}
